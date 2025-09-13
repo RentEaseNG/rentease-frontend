7fa5601bcdca5273a9ed0e3643b9ff3a7b54ab03
Ariyo Aresa <ariyoaresa89@gmail.com>
2025-09-13 01:52:57 -0700

    created login and register page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
 import LandingHero from '../components/LandingHero'
 
 const Home = () => {
   return (
     <>
-        <Navbar />
         <LandingHero />
     </>
   )
